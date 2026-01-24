5d36d08be35978a05a5bdc9b9561976c84554602
Geordie Rose <greatblueheron@users.noreply.github.com>
2026-01-24 10:11:41 +0000

    modified for clarity

--- dark-adventure/dark_adventure/generate_show_intro_and_outro.py
+++ dark-adventure/dark_adventure/generate_show_intro_and_outro.py
@@ -1,24 +1,22 @@
+""" generates text for intro and outro for your podcast... I recommend you read this in your own voice and record it"""
+
 import sys
 import os
-import json
-from script_generators.claude_functions import get_claude_completion, get_players
-from voice_generators.elevenlabs_voices import text_to_speech_file
-from elevenlabs.client import ElevenLabs
-from elevenlabs import VoiceSettings, play, voices
+from script_generators.claude_functions import get_claude_completion
 from dotenv import load_dotenv
 
 
-def generate_show_introduction():
+def generate_show_introduction(game_types_to_focus_on, name_of_my_podcast):
 
-    show_introduction = """Generate a brief intro script meant to be read before every episode of "Whispers in Green Static", a real-play table table role playing game focusing on Call of Cthulhu and Delta Green. 
+    show_introduction = f"""Generate a brief intro script meant to be read before every episode of "{name_of_my_podcast}", a real-play table table role playing game focusing on {game_types_to_focus_on}. 
     End the script with the phrase "and now a word from today's sponsor." """
 
     return show_introduction
 
 
-def generate_show_outro():
+def generate_show_outro(game_types_to_focus_on, name_of_my_podcast):
 
-    show_outro = """Generate a very short outro script meant to be read after every episode of "Whispers in Green Static", a real-play table table role playing game focusing on Call of Cthulhu and Delta Green. """
+    show_outro = f"""Generate a very short outro script meant to be read after every episode of "{name_of_my_podcast}", a real-play table table role playing game focusing on {game_types_to_focus_on}. """
 
     return show_outro
 
@@ -28,14 +26,18 @@ def main():
     load_dotenv()
     CLAUDE_API_KEY = os.getenv("CLAUDE_API_KEY")
 
+    # MODIFY THESE FOR YOUR CASE
+    game_types_to_focus_on = "Call of Cthulhu and Delta Green"
+    name_of_my_podcast = "Whispers in Green Static"
+
     try:
-        response = get_claude_completion(prompt=generate_show_introduction(), api_key=CLAUDE_API_KEY)
+        response = get_claude_completion(prompt=generate_show_introduction(game_types_to_focus_on, name_of_my_podcast), api_key=CLAUDE_API_KEY)
         print(f"Claude's response: {response}")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
     try:
-        response = get_claude_completion(prompt=generate_show_outro(), api_key=CLAUDE_API_KEY)
+        response = get_claude_completion(prompt=generate_show_outro(game_types_to_focus_on, name_of_my_podcast), api_key=CLAUDE_API_KEY)
         print(f"Claude's response: {response}")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
